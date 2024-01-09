7916de6f79d66cf7058d421e73b4878206c14e93
aymansuhail <81426384+aymansuhail@users.noreply.github.com>
2024-01-09 20:01:23 +0530

    week 6.1 wrapper components

--- App.jsx
+++ App.jsx
@@ -2,50 +2,34 @@ import { useState } from "react"
 
 
 function App() {
-  const[todo,settodo] = useState([{
-    id : 1,
-    Title : "goto gym",
-    Description : "10-11"
-  },{
-    id : 2,
-    Title : "goto code",
-    Description : "11-12"
-  },
-{ id : 3,
-  Title : "goto play",
-  Description : "12-13"}])
-  
-  function addtodo() {
-    settodo([
-      ...todo,
-      {
-        id: 4,
-        Title: Math.random().toString(),
-        Description: Math.random().toString()
-      }
-    ]);
-  }
+ 
   return (
-    <div>
-    <button onClick={addtodo}>Add todo</button>
-    {todo.map(todos =>
-      <Todo Title = {todos.Title} Description = {todos.Description}></Todo>
-      
-     )}
-    </div>
-  )
-}
-
-
-function Todo(props){
-  return(
     <>
-    <h1>{props.Title}</h1>
-    <h3>{props.Description}</h3>
+    {/* <Card inner={<Logic></Logic>}></Card>
+    <Card inner={<Logic2></Logic2>}></Card> */}
+    
+    <Card>
+      Hi there i am child
+    </Card>
     </>
   )
 }
 
+function Card({children})
+{
+  return <>
+    <div style={{border : "2px solid black" , padding : 2}}>
+    {children}
+    </div>
+  </>
+}
+// function Logic (){
+//   return <>Hi iam logic</>
+// }
+
+// function Logic2(){
+//   return <>Hi iam Logic2</>
+// }
 
 
 export default App
